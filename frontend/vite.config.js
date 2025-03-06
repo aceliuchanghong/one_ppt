ebf1131127bd3e4fbe18f71b35093567a9789ca3
sisconsavior-v-1 <aceliuchanghong@gmail.com>
2025-03-06 09:30:10 +0800

    docs: 文档

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',  // 允许外部访问
+    port: 3001,       // 指定端口
+    strictPort: true, // 如果端口被占用，则会直接退出
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
